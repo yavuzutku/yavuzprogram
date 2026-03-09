4d9ab5de16aecdff2a6cb310b9d0484f159d0b09
cetinklima80-star <cetinklima80@gmail.com>
2026-03-09 23:09:03 +0300

    auto update

--- js/kelimeler.js
+++ js/kelimeler.js
@@ -390,7 +390,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent`,
         {
           method: "POST",
           headers: {
